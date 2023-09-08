10688e9a5e7f8e0c8eaae45ea9a29f075b61d5e0
dsarge10 <dylansargent10@yahoo.com>
2023-09-08 10:03:55 -0500

    Create Prefabs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float mainThrust = 100;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 10f;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -32,12 +33,18 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Press Left - THRUST");
+            //Passing rotationThrust in to allow for a positive or negative thrust. "forward or -forward".
+            ApplyRotation(rotationThrust);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Press Right - THRUST");
+            //Passing rotationThrust in to allow for a positive or negative thrust. "forward or -forward".
+            ApplyRotation(-rotationThrust);
         }
     }
 
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
 }
